0731ede7fd83c6882ee31d3e7f1036239dc8c3a7
IntrusivePenDesperateSword <35501347+IntrusivePenDesperateSword@users.noreply.github.com>
2018-11-14 08:31:09 +0100

    Formatted JSON, added background tasks
    As well as other minor improvements

--- Sap-bot/main.py
+++ Sap-bot/main.py
@@ -1,8 +1,7 @@
 import discord
 from discord.ext import commands
-import sys
-import time
-import json
+import sys, time, json, asyncio
+
 
 with open("Secret.txt", "r") as f:
     token = f.read()
@@ -14,6 +13,8 @@ if not token:
 description = '''A bot for automatic emote replacement, mimicking virtual memory. Somewhat by IntrusivePenDesperateSword#7881.'''
 
 prefix = "!"
+clock_hours = 12
+age_length = 16
 bot = commands.Bot(command_prefix=prefix, description=description)
 
 
@@ -36,6 +37,25 @@ def is_me():
     return commands.check(predicate)
 
 
+@bot.event
+async def on_reaction_add(reaction, user):
+    bot.in_serv[reaction.emoji.name]["Referenced"] = 1
+
+
+def update_age():
+    for key, value in bot.in_serv:
+        value[1] = str(value[2]) + value[1][:-1]
+        value[2] = 0
+
+
+async def clock():
+    await bot.wait_until_ready()
+
+    while not bot.is_closed:
+        update_age()
+        await asyncio.sleep(3600*clock_hours)
+
+
 @commands.command(pass_context=True)
 async def ping():
     """Shows how long the delay is"""
@@ -49,36 +69,38 @@ async def ping():
 async def save():
     bot.in_server = await bot.get_all_emojis()
     with open("emoji.json", "w") as f:
-        json.dump(str(bot.out_server) + "\n" + str(bot.in_server), f)
-    await bot.say("Saved emoji.")
+        json.dump(str(bot.out_server), f)
+    #await bot.say("Saved emoji.")
 
 
 @commands.command(pass_context=True)
 async def load():
     with open("emoji.json", "r") as f:
-        bot.out_server, bot.in_server = json.load(f)
+        bot.out_server = json.load(f)
 
     await bot.say("Loaded emoji.")
 
 
 @commands.command(pass_context=True)
 async def add(emojiname):
-    worst = ["", "", "11111111111"]
+    worst = {"emoji":None, "Age":"1"*age_length, "Referenced":0}
     # assert emojiname is in in_server
 
     worstInd = 0
     for i in range(len(bot.in_server)):
-        if bot.in_server[i][2] < worst[2]:  # Convert to binary somehow?
-            worst = bot.in_server[i]
+        if bot.in_server[i][1] < worst[1]:
+            worst = bot.in_serv[i]
             worstInd = i
     await bot.say(f"Removing {worst[0]}, and adding {emojiname}...")
     new = []
     bot.out_server.append(bot.in_server.pop(worstInd))
+    await save()
+
     for i in bot.out_server:
         if i[0] == emojiname:
             new = i
 
-    discord.addEmoji(new)  # Not the right format but whatever
+    #discord.addEmoji(new)  # Not the right format but whatever
 
 
 @bot.command(hidden=True)
@@ -90,5 +112,5 @@ async def logout():
     print("logged out")
     sys.exit(0)
 
-
+bot.loop.create_task(clock())
 bot.run(token)
