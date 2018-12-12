75a250cbf03971829be9db39298e348f0a75870e
Sebastian Bekkvik Aas <sebasa18@student.uia.no>
2018-12-12 15:12:02 +0100

    Removed adaptation to multiple servers

--- Sap-bot/main.py
+++ Sap-bot/main.py
@@ -23,14 +23,11 @@ bot = commands.Bot(command_prefix=prefix, description=description)
 
 bot.time = datetime.datetime.now()
 
-bot.in_server = {i.id: {} for i in list(bot.servers)}
-bot.out_server = {i.id: {} for i in list(bot.servers)}
+bot.in_server = {}
+bot.out_server = {}
 
-
-bot.files = []
 with open("emojiTest.json", "r") as f:
     bot.testFile = json.load(f)
-    bot.files.append((bot.testFile, "emojiTest.json"))
 
 @bot.event
 async def on_ready():
@@ -39,23 +36,16 @@ async def on_ready():
     await bot.change_presence(game=discord.Game(name=f"For info: {prefix}help"))
     print(f'Logged in as: \n{bot.user.name}\n{bot.user.id}\nwith {bot.owner.display_name} as owner\n------')
 
-    bot.in_server = {j.id: {i.name: {"Emoji": i.id, "URL": i.url, "Age": "0" * age_length, "Referenced": 0}
-                 for i in bot.get_all_emojis() if i.server.id == j.id} for j in list(bot.servers)}
-    bot.out_server = {i.id: {} for i in list(bot.servers)}
+    bot.in_server = {i.name: {"Emoji": i.id, "URL": i.url, "Age": "0" * age_length, "Referenced": 0}
+                 for i in bot.get_all_emojis()}
 
-    print("ID is", list(bot.out_server.keys()))
-
-    for h in list(bot.servers):
-        for i in bot.testFile.keys():
-            if i in [j.name for j in bot.get_all_emojis() if j.server.id == h.id]:
-                bot.in_server[h.id][i] = bot.testFile[i]
-            else:
-                bot.out_server[h.id][i] = bot.testFile[i]
-
-    print(list(bot.out_server.keys()))
+    for i in bot.testFile.keys():
+        if i in [j.name for j in bot.get_all_emojis()]:
+            bot.in_server[i] = bot.testFile[i]
+        else:
+            bot.out_server[i] = bot.testFile[i]
 
     print(list(bot.in_server.keys()), list(bot.out_server.keys()))
-    print(list(bot.in_server[list(bot.servers)[0].id].keys()))
     await save()
 
 
@@ -87,32 +77,32 @@ async def on_reaction_add(reaction, user):
         if reaction.emoji.name not in bot.in_server.keys() :
             print("Emoji probably from outside the server used. Ignore.")
             return -2
-        bot.in_server[reaction.emoji.server.id][reaction.emoji.name]["Referenced"] = 1
+        bot.in_server[reaction.emoji.name]["Referenced"] = 1
 
 
 async def update_age():
     for key in bot.in_server.keys():
-        # print(bot.in_server[key]["Referenced"])
         bot.in_server[key]["Age"] = str(bot.in_server[key]["Referenced"]) + bot.in_server[key]["Age"][:-1]
         bot.in_server[key]["Referenced"] = 0
 
 
 async def scan_logs(time):
     temp = list(bot.in_server.keys())
-    for server in list(bot.servers):
-        for channel in server.channels:
-            for message in bot.logs_from(channel, limit=200, after=time):
-                for emo in temp:
-                    if emo in message.content:
-                        bot.in_server[emo]["Referenced"] = 1
-                        temp.pop(temp.index(emo))
+    for channel in list(bot.servers)[0].channels:
+        logs = bot.logs_from(channel, limit=200, after=time)
+        print(logs)
+        for message in logs:
+            for emo in temp:
+                if emo in message.content:
+                    bot.in_server[emo]["Referenced"] = 1
+                    temp.pop(temp.index(emo))
 
 
 async def clock():
     await bot.wait_until_ready()
 
     while not bot.is_closed:
-        #await scan_logs(bot.time)
+        await scan_logs(bot.time)
         bot.time = datetime.datetime.now()
         await update_age()
         await save()
@@ -138,44 +128,24 @@ async def test(ctx):
 
 async def save():
     """Saves emojis to the testFile."""
-    for i in range(len(bot.files)):
-        with open(bot.files[i][1], "w") as f:
-            curid = list(bot.servers)[i].id
-            print(list(bot.in_server[curid].keys()), list(bot.out_server[curid].keys()))
-            json.dump({**bot.in_server[curid],
-                       **bot.out_server[curid]}, f, indent=4)
+    with open("emojiTest.json", "w") as f:
+        json.dump({**bot.in_server, **bot.out_server}, f, indent=4)
     print("Saved.")
 
-
-"""async def load():
-    Gets unloaded emojis from the testFile and loaded from Discord.
-    bot.in_server = {i.name: {"Emoji": i.id, "URL": i.url, "Age": "0" * age_length, "Referenced": 0}
-                     for i in bot.get_all_emojis()}
-
-    with open("emojiTest.json", "r") as f:
-        bot.file = json.load(f)
-
-    for i in bot.file.keys():
-        if i in [i.name for i in bot.get_all_emojis()]:
-            bot.in_server[i] = bot.file[i]
-        else:
-            bot.out_server[i] = bot.file[i]
-
-    await save()"""
 
 @bot.command(pass_context=True)
 @commands.check(emoji_permission)
 async def unload(ctx, *emojinames):
     """Puts an emoji in the server, out of it. For debugging."""
     for emojiname in emojinames:
-        if emojiname not in bot.in_server[ctx.message.server.id].keys():
+        if emojiname not in bot.in_server.keys():
             await bot.say(f"The emoji {emojiname} didn't appear to be loaded. Maybe you misspelled?")
             continue
-        if emojiname in bot.out_server[ctx.message.server.id].keys():
+        if emojiname in bot.out_server.keys():
             await bot.say(f"{emojiname} is already unloaded.")
             continue
 
-        bot.out_server[ctx.message.server.id][emojiname] = bot.in_server[ctx.message.server.id].pop(emojiname)
+        bot.out_server[emojiname] = bot.in_server.pop(emojiname)
 
         await bot.delete_custom_emoji(discord.utils.get(ctx.message.server.emojis, name=emojiname))
 
@@ -185,21 +155,21 @@ async def unload(ctx, *emojinames):
 
 @bot.command(pass_context=True)
 async def add(ctx, *emojinames):
-    """Loads the requested emoji, and unloads one that hasn't been used in a while."""
+    """Loads the requested emojis, and unloads ones that haven't been used in a while."""
     server_id = ctx.message.server.id
     for emojiname in emojinames:
-        if emojiname in bot.in_server[server_id].keys():
+        if emojiname in bot.in_server.keys():
             await bot.say(f"The emoji {emojiname} is already loaded.")
             continue
-        if emojiname not in bot.out_server[server_id].keys():
+        if emojiname not in bot.out_server.keys():
             await bot.say(f"The emoji {emojiname} is not an unloaded emoji! Did you spell it correctly?")
             continue
 
-        if len([i for i in bot.get_all_emojis() if i.server.id == server_id]) > 48:
+        if len([i for i in bot.get_all_emojis()]) > 48:
             worst = {"worst": {"Emoji": 0, "URL": "no", "Age": "1" * age_length, "Referenced": 0}}
-            for key in bot.in_server[server_id].keys():
-                if bot.in_server[server_id][key]["Age"] <= worst[list(worst.keys())[0]]["Age"]:
-                    worst = {key: bot.in_server[server_id][key]}
+            for key in bot.in_server.keys():
+                if bot.in_server[key]["Age"] <= worst[list(worst.keys())[0]]["Age"]:
+                    worst = {key: bot.in_server[key]}
             await bot.say(f'Removing {list(worst.keys())[0]}, and adding {emojiname}...')
 
             bot.out_server[list(worst.keys())[0]] = bot.in_server.pop(list(worst.keys())[0])
@@ -209,12 +179,12 @@ async def add(ctx, *emojinames):
             await bot.say(f'Adding {emojiname}...')
 
         async with aiohttp.ClientSession() as ses:
-            async with ses.get(bot.out_server[server_id][emojiname]["URL"]) as r:
+            async with ses.get(bot.out_server[emojiname]["URL"]) as r:
                 img = await r.read()
 
         temp = await bot.create_custom_emoji(server=ctx.message.server, name=emojiname, image=img)
-        prev = bot.out_server[server_id].pop(emojiname)
-        bot.in_server[server_id][emojiname] = {"Emoji": temp.id, "URL": temp.url, "Age": prev["Age"], "Referenced": 0}
+        prev = bot.out_server.pop(emojiname)
+        bot.in_server[emojiname] = {"Emoji": temp.id, "URL": temp.url, "Age": prev["Age"], "Referenced": 0}
 
     await save()
 
